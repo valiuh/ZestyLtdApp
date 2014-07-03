71ea0b292172780e315412b4a9100cebe3cc751a
afcgold <georgegoldhagen@Georges-MacBook-Air.local>
2014-07-03 09:27:33 +0100

    Initial Commit

---
+++ ZestyApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ZestyApp
+//
+//  Created by George Goldhagen on 03/07/2014.
+//  Copyright (c) 2014 ZestyLtd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ZestyApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZestyApp
+//
+//  Created by George Goldhagen on 03/07/2014.
+//  Copyright (c) 2014 ZestyLtd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
